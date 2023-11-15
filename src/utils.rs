d70bc7da9f047297bbec958fa29acc94b203a767
Andrew Jeffery <dev@jeffas.io>
2023-11-15 15:08:11 +0000

    Fix modelling to avoid randomness

--- src/utils.rs
+++ src/utils.rs
@@ -3,15 +3,28 @@ use time::OffsetDateTime;
 
 use crate::resources::Metadata;
 
-pub fn new_uid() -> String {
+#[cfg(feature = "serve")]
+pub fn new_uid(_name:&str) -> String {
     uuid::Uuid::new_v4().to_string()
 }
 
+#[cfg(feature = "model")]
+pub fn new_uid(name:&str) -> String {
+    name.to_owned()
+}
+
+#[cfg(feature = "serve")]
 pub fn now() -> Time {
     Time(OffsetDateTime::now_utc())
 }
 
+#[cfg(feature = "model")]
+pub fn now() -> Time {
+    Time(OffsetDateTime::UNIX_EPOCH)
+}
+
 pub fn metadata(name: String) -> Metadata {
+    let uid = new_uid(&name);
     Metadata {
         name,
         generate_name: String::new(),
@@ -19,7 +32,7 @@ pub fn metadata(name: String) -> Metadata {
         creation_timestamp: None,
         deletion_timestamp: None,
         generation: 0,
-        uid: new_uid(),
+        uid,
         labels: Default::default(),
         annotations: Default::default(),
         deletion_grace_period_seconds: None,
