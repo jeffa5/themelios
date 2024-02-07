aed16fe01af86cf0f1b683ed1ca5d816191c97b1
Andrew Jeffery <dev@jeffas.io>
2024-02-07 13:59:13 +0000

    Add more endpoints for deployment

--- src/serve_cluster.rs
+++ src/serve_cluster.rs
@@ -5,8 +5,11 @@ use crate::api::SerializableResource;
 use crate::resources::Deployment;
 use crate::resources::Pod;
 use crate::state::StateView;
+use axum::extract::Path;
 use axum::extract::State;
 use axum::http::HeaderMap;
+use axum::routing::delete;
+use axum::routing::put;
 use axum::{
     http::{Method, StatusCode, Uri},
     routing::get,
@@ -16,6 +19,7 @@ use axum::{
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::APIGroup;
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::APIGroupList;
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::GroupVersionForDiscovery;
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::Status;
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::{APIResourceList, ListMeta};
 use k8s_openapi::List;
 use tokio::sync::Mutex;
@@ -67,7 +71,10 @@ fn namespaces_apps_v1() -> Router<AppState> {
 fn resources_apps_v1() -> Router<AppState> {
     Router::new()
         .route("/deployments", get(list_deployments))
+        .route("/deployments/:name", get(get_deployment))
         .route("/deployments", post(create_deployment))
+        .route("/deployments/:name", put(update_deployment))
+        .route("/deployments/:name", delete(delete_deployment))
 }
 
 #[tracing::instrument(skip_all)]
@@ -96,6 +103,26 @@ async fn list_deployments(
 }
 
 #[tracing::instrument(skip_all)]
+async fn get_deployment(
+    State(state): State<AppState>,
+    Path(name): Path<String>,
+) -> (StatusCode, Json<SerializableResource<Deployment>>) {
+    info!("Got get request for deployment");
+    let state = state.lock().await;
+    if let Some(deployment) = state.deployments.get(&name) {
+        (
+            StatusCode::OK,
+            Json(SerializableResource::new(deployment.clone())),
+        )
+    } else {
+        (
+            StatusCode::NOT_FOUND,
+            Json(SerializableResource::new(Deployment::default())),
+        )
+    }
+}
+
+#[tracing::instrument(skip_all)]
 async fn create_deployment(
     State(state): State<AppState>,
     Json(deployment): Json<Deployment>,
@@ -108,6 +135,43 @@ async fn create_deployment(
     s.deployments.insert(deployment, revision).unwrap();
     let deployment = s.deployments.get(&deployment_name).unwrap().clone();
     (StatusCode::OK, Json(deployment))
+}
+
+#[tracing::instrument(skip_all)]
+async fn update_deployment(
+    State(state): State<AppState>,
+    Json(deployment): Json<Deployment>,
+) -> (StatusCode, Json<Deployment>) {
+    info!("Got create request for deployment");
+    let mut s = state.lock().await;
+    s.revision = s.revision.clone().increment();
+    let revision = s.revision.clone();
+    let deployment_name = deployment.metadata.name.clone();
+    s.deployments.insert(deployment, revision).unwrap();
+    let deployment = s.deployments.get(&deployment_name).unwrap().clone();
+    (StatusCode::OK, Json(deployment))
+}
+
+#[tracing::instrument(skip_all)]
+async fn delete_deployment(
+    State(state): State<AppState>,
+    Path(name): Path<String>,
+) -> (StatusCode, Json<Status>) {
+    info!("Got create request for deployment");
+    let mut s = state.lock().await;
+    s.revision = s.revision.clone().increment();
+    s.deployments.remove(&name);
+    (
+        StatusCode::OK,
+        Json(Status {
+            code: None,
+            details: None,
+            message: None,
+            metadata: ListMeta::default(),
+            reason: None,
+            status: Some("Success".to_owned()),
+        }),
+    )
 }
 
 #[tracing::instrument(skip_all)]
