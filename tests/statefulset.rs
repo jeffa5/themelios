c179187c0e2246279a3358e70dc84533cfb3c8b6
Andrew Jeffery <dev@jeffas.io>
2024-02-16 10:51:38 +0000

    Merge statefulset test properties into model

--- tests/statefulset.rs
+++ tests/statefulset.rs
@@ -25,14 +25,52 @@ fn model(
     nodes: usize,
 ) -> OrchestrationModelCfg {
     let initial_state = RawState::default().with_statefulset(statefulset);
-    OrchestrationModelCfg {
+    let mut model = OrchestrationModelCfg {
         initial_state,
         statefulset_controllers: 1,
         schedulers: 1,
         nodes,
         client_state: client_actions,
         ..Default::default()
-    }
+    };
+    model.add_property(
+        Expectation::Always,
+        "statefulset status counters are correct",
+        |_model, s| {
+            let s = s.latest();
+            for sts in s.statefulsets.iter() {
+                let pods = s.pods.matching(&sts.spec.selector);
+                let mut pod_count = 0;
+                let mut ready_replicas = 0;
+                let mut active_replicas = 0;
+                for pod in pods {
+                    pod_count += 1;
+                    if is_pod_ready(pod) {
+                        ready_replicas += 1;
+                    }
+                    if is_pod_active(pod) {
+                        active_replicas += 1;
+                    }
+                }
+
+                dbg!(
+                    &s.revision,
+                    &sts.status,
+                    pod_count,
+                    ready_replicas,
+                    active_replicas
+                );
+                let satisfied = sts.status.replicas == pod_count
+                    && sts.status.ready_replicas == ready_replicas
+                    && sts.status.available_replicas == active_replicas;
+                if !satisfied {
+                    return false;
+                }
+            }
+            true
+        },
+    );
+    model
 }
 
 fn new_statefulset(name: &str, _namespace: &str, replicas: u32) -> StatefulSet {
@@ -74,7 +112,7 @@ fn test_spec_replicas_change() {
         .annotations
         .insert("test".to_owned(), "should-copy-to-replica-set".to_owned());
 
-    let mut m = model(
+    let m = model(
         statefulset,
         // scale from initial 2 up to 3 and down to 0 then up to 2 again
         ClientState::new_unordered()
@@ -82,15 +120,7 @@ fn test_spec_replicas_change() {
             .with_scale_downs(3),
         1,
     );
-    m.add_property(
-        Expectation::Eventually,
-        "new replicaset is created",
-        |_model, s| {
-            let s = s.latest();
-            let mut deployment_iter = s.deployments.iter();
-            deployment_iter.all(|d| s.replicasets.for_controller(&d.metadata.uid).count() != 0)
-        },
-    );
+    // TODO: fix up what this test is supposed to be doing
     run(m, common::CheckMode::Bfs, function_name!())
 }
 
@@ -99,44 +129,8 @@ fn test_spec_replicas_change() {
 fn test_statefulset_available() {
     let statefulset = new_statefulset("sts", "", 4);
 
-    let mut m = model(statefulset, ClientState::new_unordered(), 1);
-    m.add_property(
-        Expectation::Always,
-        "statefulset status counters are correct",
-        |_model, s| {
-            let s = s.latest();
-            for sts in s.statefulsets.iter() {
-                let pods = s.pods.matching(&sts.spec.selector);
-                let mut pod_count = 0;
-                let mut ready_replicas = 0;
-                let mut active_replicas = 0;
-                for pod in pods {
-                    pod_count += 1;
-                    if is_pod_ready(pod) {
-                        ready_replicas += 1;
-                    }
-                    if is_pod_active(pod) {
-                        active_replicas += 1;
-                    }
-                }
-
-                dbg!(
-                    &s.revision,
-                    &sts.status,
-                    pod_count,
-                    ready_replicas,
-                    active_replicas
-                );
-                let satisfied = sts.status.replicas == pod_count
-                    && sts.status.ready_replicas == ready_replicas
-                    && sts.status.available_replicas == active_replicas;
-                if !satisfied {
-                    return false;
-                }
-            }
-            true
-        },
-    );
+    let m = model(statefulset, ClientState::new_unordered(), 1);
+    // TODO: fix up what this test is supposed to be doing
     run(m, common::CheckMode::Bfs, function_name!())
 }
 
