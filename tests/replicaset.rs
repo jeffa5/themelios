857b2330662cde9e30bc3414c2b8ebd1ea5df02b
Andrew Jeffery <dev@jeffas.io>
2024-02-16 10:49:31 +0000

    Merge replicaset test properties into model

--- tests/replicaset.rs
+++ tests/replicaset.rs
@@ -18,14 +18,58 @@ mod common;
 
 fn model(replicaset: ReplicaSet, client_state: ClientState) -> OrchestrationModelCfg {
     let initial_state = RawState::default().with_replicaset(replicaset);
-    OrchestrationModelCfg {
+    let mut model = OrchestrationModelCfg {
         initial_state,
         replicaset_controllers: 1,
         schedulers: 1,
         nodes: 1,
         client_state,
         ..Default::default()
-    }
+    };
+    model.add_property(
+        Expectation::Always,
+        "when synced, replicas are created or removed to match",
+        |_model, s| {
+            let s = s.latest();
+            let mut replicasets_iter = s.replicasets.iter();
+            replicasets_iter.all(|r| {
+                let pod_count = s.pods.for_controller(&r.metadata.uid).count();
+                // when the resource has finished processing towards the desired state the
+                // status should match the desired number of replicas and the pods should match
+                // that too
+                s.resource_stable(r).implies(
+                    // the status has been updated correctly
+                    r.spec.replicas.unwrap() == r.status.replicas
+                        // and the pods were created
+                        && pod_count as u32 == r.status.replicas,
+                )
+            })
+        },
+    );
+    model.add_property(
+        Expectation::Always,
+        "when stable, all pods are created",
+        |_model, s| {
+            let s = s.latest();
+            let all_stable = s.resources_stable(&s.replicasets);
+            let expected_count = s.replicasets.iter().map(|r| r.status.replicas).sum::<u32>();
+            all_stable.implies(expected_count == s.pods.len() as u32)
+        },
+    );
+    model.add_property(
+        Expectation::Always,
+        "when stable, status replicas == spec replicas",
+        |_model, s| {
+            let s = s.latest();
+            let mut replicasets_iter = s.replicasets.iter();
+            replicasets_iter.all(|r| {
+                let stable = s.resource_stable(r);
+                let replicas_equal = r.spec.replicas.unwrap() == r.status.replicas;
+                stable.implies(replicas_equal)
+            })
+        },
+    );
+    model
 }
 
 fn model_multiple(
@@ -82,31 +126,11 @@ fn test_spec_replicas_change() {
         .annotations
         .insert("test".to_owned(), "should-copy-to-replica-set".to_owned());
 
-    let mut m = model(
+    let m = model(
         replicaset,
         ClientState::new_unordered()
             .with_scale_ups(1)
             .with_scale_downs(1),
-    );
-    m.add_property(
-        Expectation::Always,
-        "when synced, replicas are created or removed to match",
-        |_model, s| {
-            let s = s.latest();
-            let mut replicasets_iter = s.replicasets.iter();
-            replicasets_iter.all(|r| {
-                let pod_count = s.pods.for_controller(&r.metadata.uid).count();
-                // when the resource has finished processing towards the desired state the
-                // status should match the desired number of replicas and the pods should match
-                // that too
-                s.resource_stable(r).implies(
-                    // the status has been updated correctly
-                    r.spec.replicas.unwrap() == r.status.replicas
-                        // and the pods were created
-                        && pod_count as u32 == r.status.replicas,
-                )
-            })
-        },
     );
     run(m, common::CheckMode::Bfs, function_name!())
 }
@@ -117,32 +141,9 @@ fn test_overlapping_rss() {
     let replicaset_1 = new_replicaset("test-overlapping-rss-1", "", 1);
     let replicaset_2 = new_replicaset("test-overlapping-rss-2", "", 2);
 
-    let mut m = model_multiple(
+    let m = model_multiple(
         [replicaset_1, replicaset_2].into_iter(),
         ClientState::default(),
-    );
-    m.add_property(
-        Expectation::Always,
-        "when stable, all pods are created",
-        |_model, s| {
-            let s = s.latest();
-            let all_stable = s.resources_stable(&s.replicasets);
-            let expected_count = s.replicasets.iter().map(|r| r.status.replicas).sum::<u32>();
-            all_stable.implies(expected_count == s.pods.len() as u32)
-        },
-    );
-    m.add_property(
-        Expectation::Always,
-        "when stable, status replicas == spec replicas",
-        |_model, s| {
-            let s = s.latest();
-            let mut replicasets_iter = s.replicasets.iter();
-            replicasets_iter.all(|r| {
-                let stable = s.resource_stable(r);
-                let replicas_equal = r.spec.replicas.unwrap() == r.status.replicas;
-                stable.implies(replicas_equal)
-            })
-        },
     );
     run(m, common::CheckMode::Bfs, function_name!())
 }
