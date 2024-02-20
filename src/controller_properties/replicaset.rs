51e18f168ed44a22c6358126abebb81c6ff9f62e
Andrew Jeffery <dev@jeffas.io>
2024-02-20 14:41:02 +0000

    Update replicaset property to encompass only active pods

--- src/controller_properties/replicaset.rs
+++ src/controller_properties/replicaset.rs
@@ -1,6 +1,9 @@
 use stateright::Expectation;
 
-use crate::{controller::ReplicaSetController, utils::LogicalBoolExt};
+use crate::{
+    controller::{util::is_pod_active, ReplicaSetController},
+    utils::LogicalBoolExt,
+};
 
 use super::{ControllerProperties, Properties};
 
@@ -9,12 +12,20 @@ impl ControllerProperties for ReplicaSetController {
         let mut properties = Properties::default();
         properties.add(
             Expectation::Always,
-            "rs: when synced, replicas are created or removed to match",
+            "rs: when stable, status.replicas == count(active_pods)",
             |_model, s| {
                 let s = s.latest();
-                let mut replicasets_iter = s.replicasets.iter();
-                replicasets_iter.all(|r| {
-                    let pod_count = s.pods.for_controller(&r.metadata.uid).count();
+                s.replicasets.iter().all(|r| {
+                    // Despite the reference docs saying that the replicas field is
+                    // quote: Replicas is the most recently oberved number of replicas.
+                    // from: https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.24/#replicasetstatus-v1-apps
+                    // It is only actually supposed to count the number of active pods, based on the
+                    // implementation.
+                    let pod_count = s
+                        .pods
+                        .for_controller(&r.metadata.uid)
+                        .filter(|p| is_pod_active(p))
+                        .count();
                     // when the resource has finished processing towards the desired state the
                     // status should match the desired number of replicas and the pods should match
                     // that too
@@ -25,16 +36,6 @@ impl ControllerProperties for ReplicaSetController {
                         && pod_count as u32 == r.status.replicas,
                     )
                 })
-            },
-        );
-        properties.add(
-            Expectation::Always,
-            "rs: when stable, all pods are created",
-            |_model, s| {
-                let s = s.latest();
-                let all_stable = s.resources_stable(&s.replicasets);
-                let expected_count = s.replicasets.iter().map(|r| r.status.replicas).sum::<u32>();
-                all_stable.implies(expected_count == s.pods.len() as u32)
             },
         );
         properties.add(
