5b91de5575dfe24c9d9280a4eec39dfdb722fb3a
Andrew Jeffery <dev@jeffas.io>
2024-02-20 14:37:22 +0000

    Use simulation for cargo tests

--- cargo-test.sh
+++ cargo-test.sh
@@ -5,10 +5,10 @@ set -euxo pipefail
 rm -f cargo-*.out
 
 # run with default setup for tests, just dfs to reduce memory usage
-MCO_CHECK_MODE=dfs cargo test --release -- --nocapture 2>&1 | tee cargo-default.out
+MCO_CHECK_MODE=simulation cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-default.out
 
 # check with linearizability
-MCO_CHECK_MODE=dfs MCO_CONSISTENCY=linearizable cargo test --release -- --nocapture 2>&1 | tee cargo-linearizable.out
+MCO_CHECK_MODE=simulation MCO_CONSISTENCY=linearizable cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-linearizable.out
 
 # check with session reads consistency
-MCO_CHECK_MODE=dfs MCO_CONSISTENCY=session cargo test --release -- --nocapture 2>&1 | tee cargo-session.out
+MCO_CHECK_MODE=simulation MCO_CONSISTENCY=session cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-session.out
