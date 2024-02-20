d80594296b84b18c8d27a0699fb151c2dcd32264
Andrew Jeffery <dev@jeffas.io>
2024-02-20 14:41:19 +0000

    Remove unused function

--- src/state.rs
+++ src/state.rs
@@ -412,13 +412,6 @@ impl StateView {
                     && self.revision == resource.metadata().resource_version.as_str().try_into().unwrap()
     }
 
-    pub fn resources_stable<'a, T: Meta + ObservedGeneration + 'a>(
-        &self,
-        resources: impl IntoIterator<Item = &'a T>,
-    ) -> bool {
-        resources.into_iter().all(|r| self.resource_stable(r))
-    }
-
     pub fn resource_current<T: Meta>(&self, resource: &T) -> bool {
         // no other things have happened in the cluster since the update (e.g. a
         // node dying which happens to remove pods)
