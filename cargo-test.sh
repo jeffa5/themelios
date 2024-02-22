3d17f4f03b04c8eb1b540094257d5d25a3b8fd76
Andrew Jeffery <dev@jeffas.io>
2024-02-22 15:02:00 +0000

    Remove default cargo test

--- cargo-test.sh
+++ cargo-test.sh
@@ -4,9 +4,6 @@ set -euxo pipefail
 
 rm -f cargo-*.out
 
-# run with default setup for tests, just dfs to reduce memory usage
-MCO_CHECK_MODE=simulation cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-default.out
-
 # check with linearizability
 MCO_CHECK_MODE=simulation MCO_CONSISTENCY=linearizable cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-linearizable.out
 
