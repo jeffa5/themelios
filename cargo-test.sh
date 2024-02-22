fc9478455b2e0c8118722a76e0b7737bf114f840
Andrew Jeffery <dev@jeffas.io>
2024-02-22 19:44:45 +0000

    Add monotonic-session tests to cargo-test

--- cargo-test.sh
+++ cargo-test.sh
@@ -7,5 +7,8 @@ rm -f cargo-*.out
 # check with linearizability
 MCO_CHECK_MODE=simulation MCO_CONSISTENCY=linearizable cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-linearizable.out
 
+# check with monotonic (safe) session reads consistency
+MCO_CHECK_MODE=simulation MCO_CONSISTENCY=monotonic-session cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-monotonic-session.out
+
 # check with session reads consistency
 MCO_CHECK_MODE=simulation MCO_CONSISTENCY=session cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-session.out
