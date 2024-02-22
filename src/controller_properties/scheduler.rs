adf15dfeda7027133927b212852352cfebbf1ef3
Andrew Jeffery <dev@jeffas.io>
2024-02-22 14:09:42 +0000

    Remove scheduler property
    There would always be a case where we can't host all pods.

--- src/controller_properties/scheduler.rs
+++ src/controller_properties/scheduler.rs
@@ -7,15 +7,14 @@ use super::{ControllerProperties, Properties};
 impl ControllerProperties for SchedulerController {
     fn properties() -> Properties {
         let mut properties = Properties::default();
-        properties.add(
-            Expectation::Eventually,
-            "sched: every pod gets scheduled",
-            |_model, state| {
-                let state = state.latest();
-                let mut pods_iter = state.pods.iter();
-                pods_iter.all(|pod| pod.spec.node_name.is_some())
-            },
-        );
+        // properties.add(
+        //     Expectation::Eventually,
+        //     "sched: every pod gets scheduled",
+        //     |_model, state| {
+        //         let state = state.latest();
+        //         state.pods.iter().all(|pod| pod.spec.node_name.is_some())
+        //     },
+        // );
         properties
     }
 }
