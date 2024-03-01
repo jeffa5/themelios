05ad2dff716070a820a3bb9e1c25372bd255f78f
Andrew Jeffery <dev@jeffas.io>
2024-03-01 15:41:05 +0000

    Set path for test outputs

--- cargo-test.sh
+++ cargo-test.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 rm -f cargo-*.out
 
-MCO_CHECK_MODE=simulation cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-test.out
+MCO_REPORT_PATH=testout MCO_CHECK_MODE=simulation cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-test.out
