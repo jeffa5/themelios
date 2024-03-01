9ade690b0509e868c16ff1b87daaa931534083fe
Andrew Jeffery <dev@jeffas.io>
2024-03-01 13:12:11 +0000

    Mark some stale reads configs as panics

--- tests/statefulset.rs
+++ tests/statefulset.rs
@@ -1,5 +1,6 @@
 use common::run;
 use common::test_table;
+use common::test_table_panic;
 use std::collections::BTreeMap;
 use stdext::function_name;
 use themelios::model::OrchestrationModelCfg;
@@ -113,11 +114,16 @@ test_table! {
     linearizable_2(ConsistencySetup::Linearizable, 2),
     monotonic_session_1(ConsistencySetup::MonotonicSession, 1),
     monotonic_session_2(ConsistencySetup::MonotonicSession, 2),
+}
+
+test_table_panic! {
+    test_stale_reads,
     resettable_session_1(ConsistencySetup::ResettableSession, 1),
     resettable_session_2(ConsistencySetup::ResettableSession, 2),
     causal_1(ConsistencySetup::Causal, 1),
     causal_2(ConsistencySetup::Causal, 2),
 }
+
 
 // https://github.com/kubernetes/kubernetes/issues/59848
 fn test_stale_reads(consistency: ConsistencySetup, controllers: usize) -> OrchestrationModelCfg {
