d84c423f0117479395ef97c98e6ee7891a0bb730
Andrew Jeffery <dev@jeffas.io>
2024-03-04 10:54:24 +0000

    Run test binaries individually

--- cargo-test.sh
+++ cargo-test.sh
@@ -2,6 +2,13 @@
 
 set -euxo pipefail
 
-rm -f cargo-*.out
+rm -f cargo-test-*.out
 
-MCO_REPORT_PATH=testout MCO_CHECK_MODE=simulation cargo test --release -- --test-threads=1 --nocapture 2>&1 | tee cargo-test.out
+function cargo_test(){
+    MCO_REPORT_PATH=testout MCO_CHECK_MODE=simulation cargo test --release --test $1 -- --test-threads=1 --nocapture 2>&1 | tee cargo-test-$1.out
+}
+
+cargo_test deployment
+cargo_test job
+cargo_test replicaset
+cargo_test statefulset
