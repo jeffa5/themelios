c899e9a1a5d0b9d21c47378e47b5da2e246af85a
Andrew Jeffery <dev@jeffas.net>
2024-03-08 10:38:17 +0000

    Rearrange node logic for updating pod

--- src/controller/node.rs
+++ src/controller/node.rs
@@ -1,15 +1,17 @@
 use std::collections::BTreeMap;
 
 use crate::abstract_model::ControllerAction;
-use crate::controller::util::is_pod_terminating;
 use crate::controller::Controller;
 use crate::resources::{
     ConditionStatus, ContainerState, ContainerStateRunning, ContainerStateTerminated,
-    ContainerStatus, Pod, PodCondition, PodConditionType, PodPhase, ResourceQuantities,
+    ContainerStateWaiting, ContainerStatus, Pod, PodCondition, PodConditionType, PodPhase,
+    ResourceQuantities,
 };
 use crate::state::revision::Revision;
 use crate::state::StateView;
 use crate::utils::now;
+
+use super::util::is_pod_active;
 
 #[derive(Clone, Debug)]
 pub struct NodeController {
@@ -58,29 +60,88 @@ impl Controller for NodeController {
                 .filter(|p| p.spec.node_name.as_ref().map_or(false, |n| n == &self.name))
                 .collect::<Vec<_>>();
 
-            // quickly start up all local pods
             for pod in pods_for_this_node {
-                if !is_pod_terminating(pod) && !local_state.running.contains(&pod.metadata.name) {
-                    local_state.running.push(pod.metadata.name.clone());
-                    let mut new_pod = pod.clone();
-                    new_pod.status.container_statuses.clear();
-                    for c in &new_pod.spec.containers {
-                        new_pod.status.container_statuses.push(ContainerStatus {
-                            name: c.name.clone(),
-                            state: ContainerState::Running(ContainerStateRunning {
-                                started_at: Some(now()),
-                            }),
-                            ready: true,
-                            image: c.image.clone(),
-                            started: true,
-                            ..Default::default()
-                        })
-                    }
-                    new_pod.status.phase = PodPhase::Running;
-                    return Some(NodeControllerAction::UpdatePod(new_pod));
-                }
+                if is_pod_active(pod) {
+                    if !local_state.running.contains(&pod.metadata.name) {
+                        local_state.running.push(pod.metadata.name.clone());
+                        let mut new_pod = pod.clone();
+                        new_pod.status.container_statuses.clear();
+                        for c in &new_pod.spec.containers {
+                            new_pod.status.container_statuses.push(ContainerStatus {
+                                name: c.name.clone(),
+                                state: ContainerState::Running(ContainerStateRunning {
+                                    started_at: Some(now()),
+                                }),
+                                last_state: ContainerState::Waiting(
+                                    ContainerStateWaiting::default(),
+                                ),
+                                ready: true,
+                                image: c.image.clone(),
+                                started: true,
+                                ..Default::default()
+                            })
+                        }
+                        new_pod.status.phase = PodPhase::Running;
+                        return Some(NodeControllerAction::UpdatePod(new_pod));
+                    } else {
+                        // already running it, monitor it
+                        let mut new_pod = pod.clone();
+                        if pod.status.container_statuses.iter().any(|cs| {
+                            matches!(
+                                cs.state,
+                                ContainerState::Terminated(ContainerStateTerminated { exit_code, .. }) if exit_code > 0
+                            )
+                        }) {
+                            new_pod.status.phase = PodPhase::Failed;
+                            new_pod.status.conditions.clear();
 
-                if pod.metadata.deletion_timestamp.is_some() {
+                            if let Some(pos) = local_state
+                                .running
+                                .iter()
+                                .position(|r| r == &pod.metadata.name)
+                            {
+                                local_state.running.remove(pos);
+                            }
+                            return Some(NodeControllerAction::UpdatePod(new_pod));
+                        } else if pod.status.container_statuses.iter().all(|cs| {
+                            matches!(
+                                cs.state,
+                                ContainerState::Terminated(ContainerStateTerminated {
+                                    exit_code: 0,
+                                    ..
+                                })
+                            )
+                        }) {
+                            new_pod.status.phase = PodPhase::Succeeded;
+                            new_pod.status.conditions.clear();
+                            if let Some(pos) = local_state
+                                .running
+                                .iter()
+                                .position(|r| r == &pod.metadata.name)
+                            {
+                                local_state.running.remove(pos);
+                            }
+                            return Some(NodeControllerAction::UpdatePod(new_pod));
+                        }else if pod.status.phase == PodPhase::Running
+                            && !new_pod.status.conditions.iter().any(|c| {
+                                c.r#type == PodConditionType::Ready
+                                    && c.status == ConditionStatus::True
+                            })
+                        {
+                            // TODO: should have an arbitrary action to mark pods running, then this relies on
+                            // that.
+                            new_pod.status.conditions.push(PodCondition {
+                                status: ConditionStatus::True,
+                                r#type: PodConditionType::Ready,
+                                last_probe_time: None,
+                                last_transition_time: None,
+                                message: None,
+                                reason: None,
+                            });
+                            return Some(NodeControllerAction::UpdatePod(new_pod));
+                        }
+                    }
+                } else if pod.metadata.deletion_timestamp.is_some() {
                     // TODO: ensure we mark containers as shutdown first?
 
                     // pod has been marked for deletion and is running on this node, forget about
@@ -93,61 +154,8 @@ impl Controller for NodeController {
                         local_state.running.remove(pos);
                     }
                     return Some(NodeControllerAction::DeletePod(pod.clone()));
-                }
-
-                let mut new_pod = pod.clone();
-                if pod.status.container_statuses.iter().any(|cs| {
-                    matches!(
-                        cs.state,
-                        ContainerState::Terminated(ContainerStateTerminated { exit_code, .. }) if exit_code > 0
-                    )
-                }) {
-                    new_pod.status.phase = PodPhase::Failed;
-                    new_pod.status.conditions.clear();
-
-                    if let Some(pos) = local_state
-                        .running
-                        .iter()
-                        .position(|r| r == &pod.metadata.name)
-                    {
-                        local_state.running.remove(pos);
-                    }
-                    return Some(NodeControllerAction::UpdatePod(new_pod));
-                }
-                if pod.status.container_statuses.iter().all(|cs| {
-                    matches!(
-                        cs.state,
-                        ContainerState::Terminated(ContainerStateTerminated { exit_code: 0, .. })
-                    )
-                }) {
-                    new_pod.status.phase = PodPhase::Succeeded;
-                    new_pod.status.conditions.clear();
-                    if let Some(pos) = local_state
-                        .running
-                        .iter()
-                        .position(|r| r == &pod.metadata.name)
-                    {
-                        local_state.running.remove(pos);
-                    }
-                    return Some(NodeControllerAction::UpdatePod(new_pod));
-                }
-
-                // TODO: should have an arbitrary action to mark pods running, then this relies on
-                // that.
-                if pod.status.phase == PodPhase::Running
-                    && !new_pod.status.conditions.iter().any(|c| {
-                        c.r#type == PodConditionType::Ready && c.status == ConditionStatus::True
-                    })
-                {
-                    new_pod.status.conditions.push(PodCondition {
-                        status: ConditionStatus::True,
-                        r#type: PodConditionType::Ready,
-                        last_probe_time: None,
-                        last_transition_time: None,
-                        message: None,
-                        reason: None,
-                    });
-                    return Some(NodeControllerAction::UpdatePod(new_pod));
+                } else {
+                    // suceeded or failed, not sure what to do here?
                 }
             }
         } else {
