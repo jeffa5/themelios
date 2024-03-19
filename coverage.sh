5fc9a8c17033927dc920361215ed398192252706
Andrew Jeffery <dev@jeffas.net>
2024-03-19 18:24:43 +0000

    Update coverage script
    Set very high timeout so it waits. Also use llvm engine.

--- coverage.sh
+++ coverage.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 rm -f coverage.out tarpaulin-report.*
 
-MCO_REPORT_PATH=coverageout MCO_CHECK_MODE=simulation cargo tarpaulin --skip-clean --release --target-dir ctarget -o html -- --test-threads=1 --nocapture 2>&1 | tee coverage.out
+MCO_REPORT_PATH=coverageout MCO_CHECK_MODE=simulation cargo tarpaulin --timeout 100000000 --engine llvm --skip-clean --release --target-dir ctarget -o html -- --test-threads=1 --nocapture 2>&1 | tee coverage.out
